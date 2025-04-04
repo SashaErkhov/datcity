8b3f6542e75e7ad6abb0d680560eb2c5e3edae2b
Alexander Erkhov <sasori654654@gmail.com>
2025-04-04 20:22:06 +0300

    add main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from pygments.lexer import words
+
+from req_import import *
+
+def main():
+    while True:
+        for _ in range(10):
+            # ...words
+            # ...
+            # ...shuffle
+            pass
+
+
+main()
---
+++ req_import.py
@@ -0,0 +1,2 @@
+import requests
+import req_func
