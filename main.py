633d8016ea1141fda1fb8ed91e4ce4f1f164db82
Alexander Erkhov <sasori654654@gmail.com>
2025-04-05 18:21:38 +0300

    hehe

--- main.py
+++ main.py
@@ -1,37 +1,59 @@
+import req_func
 from req_import import *
 
 def main():
     while True:
-        for i in range(10):
-            words = req_func.req_words()
-            for j in range(10*(i+1)):
-                data = {
-                    "data" : 0,
-                    "words" : [
-                        {
-                            "dir": 1,
-                            "id": i,
-                            "pos": [0, 0, 0]
-                        }
-                    ]
-                }
-                ok = False
-                if words['mapSize'][0] >= len(words['words'][j]):
-                    data['words'][0]['dir'] = 1
-                    ok = True
-                elif words['mapSize'][1] >= len(words['words'][j]):
-                    data['words'][0]['dir'] = 2
-                    ok = True
-                else:
-                    data['words'][0]['dir'] = 3
-                    ok = True
-                if ok:
-                    print(req_func.req_build(data))
+        words = req_func.req_words()
+        same_words = {}
+        for i in range(1,1_000):
+            same_words[i] = []
+        for i in range(len(words['words'])):
+            same_words[len(words['words'][i])].append(i)
+        supper_same_words = {}
+        for i in 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя':
+            supper_same_words[i] = [[] for i in range(1_000+2)]
+        for i in same_words:
+            for j in same_words[i]:
+                if words['words'][j][0] == words['words'][j][-1]:
+                    supper_same_words[words['words'][j][0]][i].append(j)
+        # print(supper_same_words)
+        for i in supper_same_words:
+            for j in range(len(supper_same_words[i])):
+                if len(supper_same_words[i][j])>0:
+                    pass
+                    # print(supper_same_words[i][j])
+                if len(supper_same_words[i][j]) >= 4:
+                    # print(words['words'][supper_same_words[i][j][0]])
+                    # print(words['words'][supper_same_words[i][j][1]])
+                    # print(words['words'][supper_same_words[i][j][2]])
                     data = {
-                        "data" : 1
+                        "done": False,
+                        "words": [
+                            {
+                                "dir": 1,
+                                "id": words['words'][supper_same_words[i][j][0]],
+                                "pos":[0,0,0]
+                            },
+                            {
+                                "dir": 3,
+                                "id": words['words'][supper_same_words[i][j][1]],
+                                "pos":[0,0,j-1]
+                            },
+                            {
+                                "dir": 3,
+                                "id": words['words'][supper_same_words[i][j][2]],
+                                "pos":[j-1,0,j-1]
+                            },
+                            {
+                                "dir": 1,
+                                "id": words['words'][supper_same_words[i][j][3]],
+                                "pos":[0,0,j-1]
+                            }
+                        ]
                     }
-                    print(req_func.req_build(data))
-            req_func.req_shuffle()
+                    req_func.req_build(data)
+        # print('____')
+
 
 
 
